62083df830163e259e9b226f3cc82e60216ec178
notrace <cy_nforget@126.com>
2016-11-01 14:28:47 +0800

    add scrolltextview

---
+++ CustomView/app/src/main/java/com/notrace/ScrollTextViewActivity.java
@@ -0,0 +1,20 @@
+package com.notrace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.notrace.library.ScrollTextView;
+
+public class ScrollTextViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ScrollTextView textView=new ScrollTextView(this);
+        textView.setText("23333");
+        textView.setRndDuration(10000);
+        textView.startScroll();
+
+        setContentView(textView);
+    }
+}
